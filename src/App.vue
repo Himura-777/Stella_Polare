12c7344f415e1daedc310598fb0b7f792fa20f53
Tuan Anh <tp.anhh7@gmail.com>
2024-12-09 12:46:31 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import TheFooter from './components/TheFooter.vue'
+import TheHeader from './components/TheHeader.vue'
+</script>
+
+<template>
+  <div class="bg-[#090C11] p-12">
+    <header>
+      <TheHeader />
+    </header>
+
+    <main class="pt-12 pb-20"></main>
+
+    <footer>
+      <TheFooter />
+    </footer>
+  </div>
+</template>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,42 @@
+<script setup>
+import Logo from './icons/logo.vue'
+</script>
+
+<template>
+  <div class="flex items-center justify-between">
+    <a href="#">
+      <div class="flex gap-x-3">
+        <Logo />
+        <p
+          class="text-2xl font-medium text-transparent bg-clip-text bg-gradient-to-r from-white to-[#ACD1FD]"
+        >
+          Stella <br />
+          Polare
+        </p>
+      </div>
+    </a>
+
+    <div class="w-[92.212%] fixed text-center">
+      <ul class="px-8 rounded-full inline-flex gap-x-10 bg-white bg-opacity-10">
+        <li class="py-5">
+          <a class="text-xl text-white text-opacity-80 font-normal" href="#">О компании</a>
+        </li>
+        <li class="py-5">
+          <a class="text-xl text-white text-opacity-80 font-normal" href="#">Контакты</a>
+        </li>
+        <li class="py-5">
+          <a class="text-xl text-white text-opacity-80 font-normal" href="#">Товары</a>
+        </li>
+        <li class="py-5">
+          <a class="text-xl text-white text-opacity-80 font-normal" href="#">Отзывы</a>
+        </li>
+      </ul>
+    </div>
+
+    <button
+      class="px-6 py-5 border-[2px] border-solid border-white border-opacity-30 text-xl text-white font-medium rounded-full"
+    >
+      Связаться с нами
+    </button>
+  </div>
+</template>
